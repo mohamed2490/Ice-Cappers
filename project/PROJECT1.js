9eb7039766a6aa2c3df5496e2ca6068d3de61699
phadaei <115961535+phadaei@users.noreply.github.com>
2022-11-16 14:48:01 -0500

    Add files via upload

---
+++ project/PROJECT1.js
@@ -0,0 +1,19 @@
+function name () {
+
+    var name = document.getElementById("myInput").value;
+
+  
+    }
+  
+
+    function mouseOver() {
+
+        document.getElementById("butn").style.color = "red";
+    
+      }
+    
+      function mouseOut() {
+    
+        document.getElementById("butn").style.color = "DarkSlateGrey";
+        
+      }
