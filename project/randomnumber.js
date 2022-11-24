5d692ba5f763ff995a85f7a7fe90a95ce1cc2340
mohamed2490 <mohamed1388@hotmail.com>
2022-11-24 17:29:14 -0500

    Update randomnumber.js

--- project/randomnumber.js
+++ project/randomnumber.js
@@ -1,5 +1,7 @@
 
 var answer = Math.floor(Math.random() * 10) + 1;
+var answer1 = Math.floor(Math.random() * 20) + 1;
+var answer2 = Math.floor(Math.random() * 30) + 1;
 var numOfguess = 0;
 var guessedNum =[];
 
@@ -73,6 +75,143 @@ function guessNumber1And10(){
   
 }
 
+function guessNumber1And20(){
+//generating a random integer from 1 to 10
+score = 0;
+  
+// take input from the user
+var a=document.getElementById("number1").value;
+var a = parseInt(a);
+
+ // Keeps taking input until it fufils conditions of being within range of random number.
+if(a<1||a>10){
+  alert("Please Enter a number Between 1 to 10");
+}
+else if((Number.isInteger(a))== false){
+  alert("The number you entered is not an integer, please enter an integer Between 1 to 10 ");
+}
+else{
+    
+      
+      
+        
+       if(a>answer1){
+        guessedNum.push(a);
+        numOfguess += 1;
+      document.getElementById("output").innerHTML ='Guess is too high';
+      document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
+      document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+      if(numOfguess>=5){
+        document.getElementById("output").innerHTML ='Out of tries';
+        document.getElementById('butn').disabled=true;
+        }
+     
+      
+     
+      }
+       else if(a<answer1){
+        guessedNum.push(a);
+        numOfguess += 1;
+        document.getElementById("output").innerHTML ='Guess is too low';
+      document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
+      document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+        if(numOfguess>=5){
+        document.getElementById("output").innerHTML ='Out of tries you scored: '+ score + " points.";
+        document.getElementById('butn').disabled=true;
+        }
+      
+      
+       }
+       else{
+        guessedNum.push(a);
+        numOfguess = 0;
+        guessedNum = [];
+        score += 1;
+        answer1 = Math.floor(Math.random() * 20) + 1;
+          document.getElementById("output").innerHTML ='Guess is correct';
+          document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
+           document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+        
+
+       }
+      
+       
+      
+  }
+
+
+
+
+}
+
+function guessNumber1And30(){
+  //generating a random integer from 1 to 30
+  score = 0;
+    
+  // take input from the user
+  var a=document.getElementById("number1").value;
+  var a = parseInt(a);
+  
+   // Keeps taking input until it fufils conditions of being within range of random number.
+  if(a<1||a>10){
+    alert("Please Enter a number Between 1 to 10");
+  }
+  else if((Number.isInteger(a))== false){
+    alert("The number you entered is not an integer, please enter an integer Between 1 to 10 ");
+  }
+  else{
+      
+        
+        
+          
+         if(a>answer2){
+          guessedNum.push(a);
+          numOfguess += 1;
+        document.getElementById("output").innerHTML ='Guess is too high';
+        document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
+        document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+        if(numOfguess>=7){
+          document.getElementById("output").innerHTML ='Out of tries';
+          document.getElementById('butn').disabled=true;
+          }
+       
+        
+       
+        }
+         else if(a<answer2){
+          guessedNum.push(a);
+          numOfguess += 1;
+          document.getElementById("output").innerHTML ='Guess is too low';
+        document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
+        document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+          if(numOfguess>=7){
+          document.getElementById("output").innerHTML ='Out of tries you scored: '+ score + " points.";
+          document.getElementById('butn').disabled=true;
+          }
+        
+        
+         }
+         else{
+          guessedNum.push(a);
+          numOfguess = 0;
+          guessedNum = [];
+          score += 1;
+          answer1 = Math.floor(Math.random() * 30) + 1;
+            document.getElementById("output").innerHTML ='Guess is correct';
+            document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
+             document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+          
+  
+         }
+        
+         
+        
+    }
+  
+  
+  
+  
+  }
 
 
 
