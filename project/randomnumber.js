17f59678706847e620ee3fbb92071c1ba2b66a0c
mohamed2490 <mohamed1388@hotmail.com>
2022-11-24 17:18:53 -0500

    correct js code

--- project/randomnumber.js
+++ project/randomnumber.js
@@ -3,10 +3,10 @@ var answer = Math.floor(Math.random() * 10) + 1;
 var numOfguess = 0;
 var guessedNum =[];
 
-function guessNumber(){
+function guessNumber1And10(){
 
   //generating a random integer from 1 to 10
-
+  score = 0;
   
   // take input from the user
  var a=document.getElementById("number1").value;
@@ -20,9 +20,9 @@ function guessNumber(){
     alert("The number you entered is not an integer, please enter an integer Between 1 to 10 ");
   }
   else{
-      do {
-        
       
+        
+        
           
          if(a>answer){
           guessedNum.push(a);
@@ -30,24 +30,42 @@ function guessNumber(){
         document.getElementById("output").innerHTML ='Guess is too high';
         document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
         document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+        if(numOfguess>=3){
+          document.getElementById("output").innerHTML ='Out of tries';
+          document.getElementById('butn').disabled=true;
+          }
+       
+        
+       
         }
          else if(a<answer){
           guessedNum.push(a);
           numOfguess += 1;
-        document.getElementById("output").innerHTML ='Guess is too low';
+          document.getElementById("output").innerHTML ='Guess is too low';
         document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
         document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
+          if(numOfguess>=3){
+          document.getElementById("output").innerHTML ='Out of tries you scored: '+ score + " points.";
+          document.getElementById('butn').disabled=true;
+          }
+        
+        
          }
          else{
           guessedNum.push(a);
-          numOfguess += 1;
+          numOfguess = 0;
+          guessedNum = [];
+          score += 1;
+          answer = Math.floor(Math.random() * 10) + 1;
             document.getElementById("output").innerHTML ='Guess is correct';
             document.getElementById("message1").innerHTML = "No. Of Guesses : " + numOfguess;
              document.getElementById("message2").innerHTML = "Guessed Number Are: " + guessedNum;
-            document.getElementById('butn').disabled=true;
+          
+
          }
         
-        } while (numOfguess<3); 
+         
+        
     }
   
   
