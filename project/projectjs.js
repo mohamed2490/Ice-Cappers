1eafd62589c21bb059e7d83656323d864cef43af
phadaei <amirf@my.yorku.ca>
2022-12-01 15:33:45 -0500

    name changes
    lol

---
+++ project/projectjs.js
@@ -0,0 +1,26 @@
+function name () 
+
+{
+
+  var name = document.getElementById("Input").value;
+  document.getElementById("myInput").innerHTML  = name;
+  
+}
+  
+
+function mouseOver()
+
+ {
+
+  document.getElementById("butn").style.color = "red";
+    
+ }
+    
+
+function mouseOut() 
+
+{
+    
+  document.getElementById("butn").style.color = "DarkSlateGrey";
+        
+}
